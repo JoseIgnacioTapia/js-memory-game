f31c38923e83c30316e448ca3851b687e0eaca53
José Ignacio <josignaciot@gmail.com>
2020-12-10 21:04:47 -0300

    position with flex

--- app.js
+++ app.js
@@ -63,6 +63,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // create you board
   function createBoard() {
+
     for (let i = 0; i < cardArray.length; i++) {
       let card = document.createElement('img');
       card.setAttribute('src', 'img/blank.png');
@@ -75,15 +76,16 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // check for matches
   function checkForMatch() {
-    const cards = document.querySelectorAll('img');
+    const cards = document.querySelectorAll('img');   // Take the img elements
     const optionOneId = cardsChosenId[0];
     const optionTwoId = cardsChosenId[1];
 
     if (cardsChosen[0] === cardsChosen[1]) {
       alert('You found a match');
-      cards[optionOneId].setAttribute('src', 'img/white.png');
-      cards[optionTwoId].setAttribute('src', 'img/white.png');
+      cards[optionOneId].setAttribute('src', 'img/white.png');   // hidden the pictures
+      cards[optionTwoId].setAttribute('src', 'img/white.png');   
       cardsWon.push(cardsChosen);
+      console.log(cardsWon);
     } else {
       cards[optionOneId].setAttribute('src', 'img/blank.png');
       cards[optionTwoId].setAttribute('src', 'img/blank.png');
@@ -94,6 +96,7 @@ document.addEventListener('DOMContentLoaded', () => {
     cardsChosenId = [];
     resultDisplay.textContent = cardsWon.length;
     if (cardsWon.length === cardArray.length/2) {
+      console.log(cardArray.length);
       resultDisplay.textContent = 'Congratulations! You found them all!';
     }
   }
@@ -103,7 +106,8 @@ document.addEventListener('DOMContentLoaded', () => {
     const cardId = this.getAttribute('data-id');
     cardsChosen.push(cardArray[cardId].name);
     cardsChosenId.push(cardId);
-    this.setAttribute('src', cardArray[cardId].img);
+    console.log(this);
+    this.setAttribute('src', cardArray[cardId].img);   // show the card 
     if (cardsChosen.length === 2) {
       setTimeout(checkForMatch, 500);
     }
